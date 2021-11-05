22e171eac456476d547f83d5fd83e4f208c99c84
Antoine75-JS <hello@antoinegarcia.com>
2021-11-05 18:21:54 +0100

    connect to db with mongoose

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ require('dotenv').config();
 
 // Dependencies
 const express = require('express');
+const mongoose = require('mongoose');
+
 const cors = require('cors');
 const routers = require('./routers');
 
 // Creates express app
 const app = express();
+
+// Connect mongo db
+mongoose.connect(
+  // "mongodb://username:password@172.25.0.2:27017/?authSource=admin"
+
+  // If service setup in docker-compose
+  "mongodb://username:password@mongodb:27017/?authSource=admin"
+).then(() => {
+  console.log("Connected to mongo database !");
+}).catch((err) => console.log(err));
 
 // App init
 app.use(express.json());
