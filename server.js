897eb4f77ea69bf55cb9894fa3208e47ef05f006
Antoine75-JS <hello@antoinegarcia.com>
2021-11-05 20:02:04 +0100

    setup env ok

--- server.js
+++ server.js
@@ -14,10 +14,14 @@ const app = express();
 
 // Connect mongo db
 const mongoUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
 mongoose
   .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to mongo database !"))
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 // App init
 app.use(express.json());
